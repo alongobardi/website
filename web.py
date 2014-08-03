ebbae51fc07d074063ed0dad6d88a9e3ff8f935b
Jonny Elliott <jonnynelliott@googlemail.com>
2014-08-03 17:44:26 +0200

    Updates

--- web.py
+++ web.py
@@ -6,5 +6,21 @@ app = Flask(__name__)
 def home():
     return render_template('home.html')
 
+@app.route('/interests/')
+def interests():
+	return render_template('interests.html')
+
+@app.route('/publications/')
+def publications():
+	return render_template('publications.html')
+
+@app.route('/cv/')
+def cv():
+	return render_template('cv.html')
+
+@app.route('/contact/')
+def contact():
+	return render_template('contact.html')
+
 if __name__ == "__main__":
   app.run()

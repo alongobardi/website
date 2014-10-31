a6216946c0bb55f7a67cf39c8474b5826824a698
Jonny Elliott <jonnynelliott@googlemail.com>
2014-10-31 15:46:47 +0100

    Changes

--- web.py
+++ web.py
@@ -13,9 +13,10 @@ def get_ads():
   #BACKUP_URL = "http://thoselondonstudents.files.wordpress.com/2013/07/dsc_0500.jpg"
                 
 
-  BACKUP_URL = "https://lh4.googleusercontent.com/-Y8gMy_GeFNM/VFOcLtFapPI/AAAAAAAAAEY/HdicNXq4yH0/w1598-h899-no/P1030294.jpg"
-
-
+  BACKUP_URL = ["https://lh4.googleusercontent.com/-Y8gMy_GeFNM/VFOcLtFapPI/AAAAAAAAAEY/HdicNXq4yH0/w1598-h899-no/P1030294.jpg"
+,"https://lh6.googleusercontent.com/-soRMKsuGS7c/VFOcHveKmAI/AAAAAAAAAEM/u4SCIp3gX2o/w1598-h899-no/P1030253.jpg"
+,"https://lh3.googleusercontent.com/-iu_9L35Ong0/VFOb_rpfIKI/AAAAAAAAAD4/1b9KD9zzCv0/w1598-h899-no/P1030193%2B-%2BVersion%2B2.jpg"
+,"https://lh5.googleusercontent.com/-ZjrwsMlhoKQ/VFOb15W1iII/AAAAAAAAAEs/lcH_S9Pb4gs/w1598-h700-no/P1030187.jpg"
   #BACK_URLS = ["one.jpg", "two.jpg"]
 
   # Obtain the webpage
@@ -31,15 +32,15 @@ def get_ads():
     image_link = search.group().replace("<IMG SRC=\"", "")
     ads_url = "{0}{1}".format(BASE_PAGE, image_link)
   except:
-    ads_url = BACKUP_URL
-    #ads_url = BACKUP_URL[0]
+    #ads_url = BACKUP_URL
+    ads_url = BACKUP_URL[0]
 
   # Add some flare
   rand = random.random()
   if rand > 0.5:
-    #rand_index = int(random.random()*len(BACK_URL))
-    ads_url = BACKUP_URL
-    #ads_url = BACKUP_URL[rand_index]
+    rand_index = int(random.random()*len(BACK_URL))
+    #ads_url = BACKUP_URL
+    ads_url = BACKUP_URL[rand_index]
 
 
   return ads_url
